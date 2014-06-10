ce4763728ed807d71cd831331b5f73a8bbab5f6d
Michael Tuttle <tuttlem@gmail.com>
2014-06-10 16:52:49 +1000

    Silly I/O decisions
    Changed the crazy idea of reading a JSON file to send back down the pipe
    to just having the web browser point to the JSON file. Idiot.

--- flashcards/views.py
+++ flashcards/views.py
@@ -11,9 +11,3 @@ def show_home():
 def show_test():
    return render_template('test.html')
 
-@app.route('/data/types')
-def retrieve_types():
-   SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-   json_url = os.path.join(SITE_ROOT, "static/data", "types.json")
-   data = json.load(open(json_url))
-   return jsonify(data)
